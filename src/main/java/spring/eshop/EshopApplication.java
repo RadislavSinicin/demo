ba3f030bf44bc24b780555e501112387a0230ee1
Rodyslav Sinitsyn <Rodyslav_Sinitsyn@epam.com>
2020-02-03 12:56:29 +0200

    Initial commit

---
+++ src/main/java/spring/eshop/EshopApplication.java
@@ -0,0 +1,12 @@
+package spring.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopApplication.class, args);
+    }
+}
